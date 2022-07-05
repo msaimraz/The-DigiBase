e043c03ac590c3566fa49b878141ae0beed38f0a
Muhammad Saim Raza <msraza.02@gmail.com>
2022-07-05 08:43:57 +0500

    DigiBase

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -25,10 +25,10 @@ class MyDocument extends Document {
 
           <meta name="author" content="M Saim Raza" />
           <link rel="shortcut icon" href="/img/favicon/favicon.ico" />
-          {/* <link rel="apple-touch-icon" sizes="180x180" href="/img/favicon/apple-touch-icon.png" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/img/favicon/apple-touch-icon.png" />
           <link rel="icon" type="image/png" sizes="32x32" href="/img/favicon/favicon-32x32.png" />
           <link rel="icon" type="image/png" sizes="16x16" href="/img/favicon/favicon-16x16.png" />
-          <link rel="manifest" href="/img/favicon/site.webmanifest" /> */}
+          <link rel="manifest" href="/img/favicon/site.webmanifest" />
           {/* Google Fonts */}
           <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&display=swap" rel="stylesheet" />
           <link href="https://fonts.googleapis.com/css2?family=Barlow:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
