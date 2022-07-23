422ff8b97a96422923e2695b51e93eb3e58ba073
Muhammad Saim Raza <msraza.02@gmail.com>
2022-07-23 11:15:01 +0500

    Update mail.php

--- src/components/Contact-section/mail.php
+++ src/components/Contact-section/mail.php
@@ -1,18 +1,18 @@
 <?php
 //get data from form  
 
-$name = $_POST['name'];
-$email = $_POST['email'];
-$message = $_POST['message'];
+// $name = $_POST['name'];
+// $email = $_POST['email'];
+// $message = $_POST['message'];
 
-$to = "msraza.02@gmail.com";
-$subject = "Mail From The DigiBase";
+// $to = "msraza.02@gmail.com";
+// $subject = "Mail From The DigiBase";
 
-$txt = "Name = " . $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
-$headers = "From: noreply@digibase.com" . "\r\n" .
-    "CC: msraza.002@gmail.com";
-if ($email != NULL) {
-    mail($to, $subject, $txt, $headers);
-}
+// $txt = "Name = " . $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
+// $headers = "From: noreply@digibase.com" . "\r\n" .
+//     "CC: msraza.002@gmail.com";
+// if ($email != NULL) {
+//     mail($to, $subject, $txt, $headers);
+// }
 //redirect
 // header("Location:thankyou.html");
