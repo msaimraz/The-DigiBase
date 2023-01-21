d6cf832fb144c44d5221f63dfa3620e1e44a5967
Muhammad Saim Raza <msraza.02@gmail.com>
2023-01-21 21:21:29 +0500

    Update Dark.jsx

--- src/layouts/Dark.jsx
+++ src/layouts/Dark.jsx
@@ -7,7 +7,18 @@ const DarkTheme = ({ children }) => {
   return (
     <>
       <Head>
-        <link rel="stylesheet" href="/css/dark.css" />
+        <>
+    <link rel="stylesheet" href="/css/dark.css" />
+    <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-EK5TN8H9G2"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-EK5TN8H9G2');
+        </script>
+  </>
       </Head>
       {children}
       
