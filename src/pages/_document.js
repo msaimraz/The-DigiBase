8a16fbb577f4b6de2084992a7a0152375bd49fcc
Muhammad Saim Raza <msraza.02@gmail.com>
2023-01-22 21:27:54 +0500

    Update _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -36,13 +36,7 @@ class MyDocument extends Document {
           <link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
           <link href="https://fonts.googleapis.com/css2?family=Barlow+Condensed:wght@200;300;400;500;600;700&display=swap" rel="stylesheet" />
 
-          <script async src="https://www.googletagmanager.com/gtag/js?id=G-EK5TN8H9G2"></script>
-          <script>
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments)}
-            gtag('js', new Date());
-            gtag('config', 'G-EK5TN8H9G2');
-          </script>
+
         </Head>
 
         <body>
